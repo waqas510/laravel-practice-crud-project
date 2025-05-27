9ad9dd41c8775ab7b5509f1735f2477a41f4256b
waqas <waqas@gmail.com>
2025-05-27 18:07:16 +0500

    waqas

---
+++ database/migrations/2025_05_27_114648_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PHPUnit\Framework\Constraint\Constraint;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('pro_name');
+            $table->integer('pro_price');
+            $table->string('pro_qty');
+            $table->text('pro_des')->nullable();
+            $table->string('pro_img');
+            $table->foreignId('cat_id')->constrained('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+<body>
+    <div class="bg-dark py-3">
+        <h3 class="text-white">Hafiz Waqas jadon</h3>
+        <h6 class="text-white">Crud practice project</h6>
+    </div>
+    
+</body>
+</html>
