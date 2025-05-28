7bfce3c8183ea29cad879f0fd53d8b47a70cf750
waqas <waqas@gmail.com>
2025-05-28 16:31:05 +0500

    form done

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -8,9 +8,35 @@
 </head>
 <body>
     <div class="bg-dark py-3">
-        <h3 class="text-white">Hafiz Waqas jadon</h3>
-        <h6 class="text-white">Crud practice project</h6>
+        <h3 class="text-white text-center">Hafiz Waqas jadon</h3>
+        <h6 class="text-white text-center">Crud practice project</h6>
     </div>
+    <div class="conatiner mt-5">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-8 border-0 shadow">
+                <div class="card-body">
+        <div class="mb-3">
+            <label class="form-label h4 mt-3">Category Name</label>
+            <input type="text" name="name" class="form-control form-control-lg" placeholder="Enter Name">
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="mb-3">
+            <label class="form-label h4">Category Description</label>
+            <textarea name="description" class="form-control form-control-lg" cols="30" rows="5" placeholder="Enter Description"></textarea>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="mb-3">
+            <label class="form-label h4">Category Image</label>
+            <input type="file" class="form-control form-control-lg" name="image">
+        </div>
+    </div>
+    <button class="btn btn-primary w-100 mb-3 btn-lg">Submit</button>
+            </div>
+        </div>
+    </div>
+    
     
 </body>
 </html>
